06294cbaecbc232ca7a4cc4679503d83baf41bdb
Yuriy-Zhitkov <zhitkov-nvc@ya.ru>
2021-05-09 11:27:57 +0300

    change files

--- temp.py
+++ temp.py
@@ -1,3 +1,14 @@
-a = 123456789
-b = 's'
-print(f'{a:02})
+a = dict(k= [1,2,3], l= [])
+
+print(a)
+
+for itm in a.items():
+    a[itm[0]] = itm[1].append(999)
+    print(itm[0])
+    print(itm[1])
+
+
+
+
+print(a)
+
