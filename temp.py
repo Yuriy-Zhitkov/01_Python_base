123f5749fe217fc17e192eb0fd75f00b8cab8224
Yuriy-Zhitkov <zhitkov-nvc@ya.ru>
2021-05-26 18:32:46 +0300

    change files

--- temp.py
+++ temp.py
@@ -1,13 +1,17 @@
+num = 11
+n = 3
 
+# lines = num // n
+# piece = num - num // n * n
+# for i in range(lines):
+#     print('*' * n)
+# print('*' * piece)
 
-class Test:
+c = ''
+for i in range(num):
+    if i % n != 0 or i == 0:
+        c += '*'
+    else:
+        c += '\n*'
 
-    def message(self, text):
-        self.text = text
-        return str(text)
-
-
-my_text = Test()
-my_text.message('qwqw')
-
-print(my_text)
+print(c)
